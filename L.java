5032efd53567bc3b925520ee6a0782720d0ae5ad
Lasya <lasyapriyamadduri@gmail.com>
2025-10-21 11:48:59 +0530

    Third java file

---
+++ L.java
@@ -0,0 +1,12 @@
+public class L
+{
+public static void main(String[] args)
+{
+int a = 18;
+b=++a;
+c=a++;
+System.out.println("b=++a",b);
+System.out.println("c=a++",c);
+System.out.println("Java file by lasya");
+}
+}
